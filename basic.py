62447120d303bb492125774fe04edd410deb4db4
Aman-Gauatam1 <amangautam1686@gmail.com>
2026-06-30 07:22:42 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,42 @@
+student={}
+
+while True:
+    print("\n-----Student Manager App------")
+    print("1. Add Student ")
+    print("2. View Student ")
+    print("3. check Result ")
+    print("4. Exit ")
+
+    choice=input("Enter your Choice : ")
+
+    if choice=="1":
+        name=input("Enter student name : ")
+        marks=int(input("Enter Marks : "))
+        student[name]=marks
+        print(f"{name} Successfully Added : ")
+
+    elif choice=="2":
+        if not student:
+            print("No Student Found !")
+        else:
+            for name,marks in student .items():
+                print(name,":",marks)
+
+    elif choice=='3':
+        name=input("Enter student Name: ")
+
+        if name in student:
+            marks=student[name]
+
+            if marks>=40:
+                print("Pass","Marks :",marks)
+            else:
+                print("Fail","Marks : ",marks)
+        else:
+            print("Student Not found !")
+
+    elif choice=="4":
+        print("Exiting......")
+        break
+    else:
+        print("Invaild Input")
